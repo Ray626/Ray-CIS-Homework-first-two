e69c5af5849aa1aa3cdc389aff8fadd3860d42b4
Ray626 <rayyu626@gmail.com>
2021-11-10 14:47:16 -0800

    expression.assignment

---
+++ src/variableAndName/Problem2.java
@@ -0,0 +1,15 @@
+package variableAndName;
+
+public class Problem2 {
+    public static void main(String[] args) {
+        int roomNum = 113;
+        double e = 2.71828;
+        String subject = "Computer Science";
+        System.out.println("This is room # " + roomNum);
+        System.out.println("e is close to" + e);
+        System.out.println("I am learning about" + subject);
+
+    }
+
+
+}
