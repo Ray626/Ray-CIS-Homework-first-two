97ecae0c3813dfca423aceff3b00ea0056b0f379
Ray626 <rayyu626@gmail.com>
2021-11-10 15:04:00 -0800

    schedule

---
+++ src/variableAndName/schedule.java
@@ -0,0 +1,14 @@
+package variableAndName;
+
+import java.sql.SQLOutput;
+
+public class schedule {
+    public static void main(String[] args) {
+        String firstAndLastLine = "+" + "-".repeat(50) + "+";
+        String block1 = "Pre_Calculus";
+        String block2 = "AP Calculus";
+        System.out.println(firstAndLastLine);
+        System.out.println(block1.length());
+        }
+    }
+
