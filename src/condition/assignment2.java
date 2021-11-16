d11dbd5184cff3af4f716eafa05d1137b8a030f8
Ray626 <rayyu626@gmail.com>
2021-11-16 14:04:13 -0800

    ifElseAssignment2

---
+++ src/condition/assignment2.java
@@ -0,0 +1,25 @@
+package condition;
+import java.util.Scanner;
+public class assignment2 {
+    public static void main(String[] args) {
+        Scanner theName = new Scanner(System.in);
+        System.out.println("Hey, what is your name?");
+        String userName = theName.nextLine();
+        System.out.println("Ok, " + userName + ", how old are your?");
+        Scanner theAge = new Scanner(System.in);
+        int userAge = theAge.nextInt();
+        if(userAge < 16){
+            System.out.println("You can't drive, "+ userName + ".");
+        }
+        else if(userAge <=17){
+            System.out.println("You can drive but not vote, " + userName + ".");
+        }
+        else if(userAge<=24){
+            System.out.println("You can drive but not renting a car, " + userName + ".");
+        }
+        else{
+            System.out.println("You can do pretty much thing, "+ userName + ".");
+        }
+        
+    }
+}
