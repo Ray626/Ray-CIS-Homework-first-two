9a5edff305afc65e500e2d3456fb10f82a7c4b79
Ray626 <rayyu626@gmail.com>
2021-11-22 14:33:19 -0800

    arrayPractice

---
+++ src/array/arrayPractice.java
@@ -0,0 +1,47 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class arrayPractice {
+    public static void main(String[] args) {
+        int[] numArray1 = new int[20];
+        Integer[] numArray2 = new Integer[10];
+        int numCount = 0;
+        int numAverage =0;
+        int swappingPosition1 = 1;
+        int swappingPosition2 = 2;
+        for(int i = 0; i<20; i++ ){
+            int randNum1 = (int)(Math.random() * 6) + 1;
+            numArray1[i] = randNum1;
+           // System.out.println(numArray1[i]);
+            if (numArray1[i]==1){
+                numCount++;
+            }
+        }
+        for(int i = 0; i<10; i++ ){
+            int randNum1 = (int)(Math.random() * 100) + 1;
+            numArray2[i] = randNum1;
+            numAverage += numArray2[i];
+        }
+        System.out.println("First array: " + Arrays.toString(numArray1));
+        System.out.println(numCount + " of 1 generated in this array");
+
+        System.out.println("Second array: " + Arrays.toString(numArray2));
+
+        int temp = numArray2[swappingPosition1];
+        numArray2[swappingPosition1] = numArray2[swappingPosition2];
+        numArray2[swappingPosition2] =temp;
+        System.out.println("Swap: " + Arrays.toString(numArray2));
+        int[] numArray2Reverse = new int[10];
+        for (int i=0; i<10; i++){
+            numArray2Reverse[i] = numArray2[9-i];
+        }
+
+        System.out.println("Reverse: " + Arrays.toString(numArray2Reverse));
+
+        System.out.println("Average of the second array: " + numAverage/10);
+
+    }
+}
