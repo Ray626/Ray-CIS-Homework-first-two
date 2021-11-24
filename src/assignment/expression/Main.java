0e8a3664dae44547005aa13303026574261421cc
Ray626 <rayyu626@gmail.com>
2021-11-24 14:45:00 -0800

    expression

--- src/assignment/expression/Main.java
+++ src/assignment/expression/Main.java
@@ -45,6 +45,7 @@ public class Main {
         System.out.println(((7 + 3) * Math.sin(45) + 2) - Math.pow(5.0, 3));
         System.out.println((10 * Math.sin(45) + 2)- 125.0);
         System.out.println(10 * 0.8509035245341184 + 2 -125.0);
+        System.out.println(8.509035245341185 + 2 - 125.0);
         System.out.println(-114.49096475465882);
     }
 }
--- src/assignment/method/methodAndArrayAssignment.java
+++ src/assignment/method/methodAndArrayAssignment.java
@@ -4,22 +4,27 @@ import java.util.Arrays;
 
 public class methodAndArrayAssignment {
     static int[] arrayAppend(int[] theArray,int appendedValue ){
-        int[] arrayTwo = new int[theArray.length+1];
-        arrayTwo[theArray.length] = appendedValue;
-        for(int i=0; i< theArray.length; i++){
-            arrayTwo[i] = theArray[i];
+        int[] arrayAdd = new int[theArray.length+1];
+        arrayAdd[theArray.length] = appendedValue;
+        for(int i = 0; i< theArray.length; i++){
+            arrayAdd[i] = theArray[i];
         }
+        return arrayAdd;
+    }
+    static int[] arrayDelete(int[] theArray){
+        int[] arrayDelete = new int[theArray.length-1];
+        for (int i = 0; i< arrayDelete.length; i++){
 
-
-        return arrayTwo;
+        }
+        return arrayDelete;
     }
     public static void main(String[] args) {
 
         int[] numArray1 = new int[1];
         numArray1[0] = 1;
         System.out.println(Arrays.toString(numArray1));
-        int[]array2 =  arrayAppend(numArray1, 2);
-        System.out.println(Arrays.toString(array2));
+        numArray1 = arrayAppend(numArray1, 2);
+        System.out.println(Arrays.toString(numArray1));
 
     }
 }
